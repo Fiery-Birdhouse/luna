6391f390a1ae7da7dcdad41003471235a665d314
Mts <goodnews.room@hotmail.com>
2017-02-18 21:05:11 -0200

    Importa algumas bibliotecas

--- conf.lua
+++ conf.lua
@@ -1,3 +1,8 @@
+-- Importa algumas das bibliotecas necessárias
+require 'lib/pl'
+require("lib/stateManager")
+require("lib/lovelyMoon")
+
 function love.conf(t)
 	t.version = "0.10.2"
 	t.window.title = "Luna Engine"
