00af02db8592fd40d5d0517e1187d1a0defd16de
Mts <goodnews.room@hotmail.com>
2017-04-01 15:59:30 -0300

    Remove métodos que não estão mais sendo utilizados

--- Luna.lua
+++ Luna.lua
@@ -15,18 +15,6 @@ Luna.settings = {
   }
 }
 
-function Luna:_init()
-	self:loadSettings()
-end
-
-function Luna:loadSettings()
-  self.settings = lip.load('settings.cfg')
-end
-
-function Luna:saveSettings()
-  lip.save('settings.cfg', self.settings)
-end
-
 function Luna:updateVideo()
   love.window.setMode(self.settings.video.w, self.settings.video.h, {vsync = self.settings.video.vsync})
 end
