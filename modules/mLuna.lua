ce5e918189cfff17a462843b909e4fdd448c32eb
Mts <goodnews.room@hotmail.com>
2018-11-25 00:06:33 -0200

    Remove global access to Luna class

--- modules/mLuna.lua
+++ modules/mLuna.lua
@@ -1,27 +1,29 @@
-class.Luna()
+local Luna = class()
 
 -- Default settings
 local videoW, videoH = love.window.getDesktopDimensions()
 Luna.settings = {
-  sound = {
-    _tweakable = {"sVolume", "mVolume"},
-    sVolume = 70,
-    mVolume = 80,
-  },
+	sound = {
+		_tweakable = {"sVolume", "mVolume"},
+		sVolume = 70,
+		mVolume = 80,
+	},
 
-  video = {
-    _tweakable = {"w", "h", "vsync", "fullscreen"},
-    w = videoW,
-    h = videoH,
-    vsync = true,
-    fullscreen = false
-  }
+	video = {
+		_tweakable = {"w", "h", "vsync", "fullscreen"},
+		w = videoW,
+		h = videoH,
+		vsync = true,
+		fullscreen = false
+	}
 }
 
 -- Update video settings with the values that player defined
 function Luna:updateVideo()
-  love.window.setMode(self.settings.video.w, self.settings.video.h, {
-    fullscreen = self.settings.video.fullscreen,
-    vsync = self.settings.video.vsync
-  })
+	love.window.setMode(self.settings.video.w, self.settings.video.h, {
+		fullscreen = self.settings.video.fullscreen,
+		vsync = self.settings.video.vsync
+	})
 end
+
+return Luna
