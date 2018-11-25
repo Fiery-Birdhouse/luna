4238c65dcd440f0c52f424c737fc94961c506767
Mts <goodnews.room@hotmail.com>
2018-11-25 00:07:26 -0200

    Update the indentation standard

--- modules/mPersistence.lua
+++ modules/mPersistence.lua
@@ -2,36 +2,36 @@
 class.Persistence()
 
 function Persistence:saveSettings(dir)
-  dir = dir or 'settings.cfg'
-    success, message = lip.save(dir, luna.settings, true)
+	dir = dir or 'settings.cfg'
+		success, message = lip.save(dir, luna.settings, true)
 
-    if not success then
-      log.warn(message)
-    end
+		if not success then
+			log.warn(message)
+		end
 end
 
 function Persistence:loadSettings(dir)
-  dir = dir or 'settings.cfg'
+	dir = dir or 'settings.cfg'
 
-  -- Check whether the specified file exists
-  if love.filesystem.getInfo(dir) ~= nil then
-    local playerSettings = lip.load(dir) -- Load player settings
+	-- Check whether the specified file exists
+	if love.filesystem.getInfo(dir) ~= nil then
+		local playerSettings = lip.load(dir) -- Load player settings
 
-    -- Iterate over INI sections
-    for section, sectionValue in pairs(playerSettings) do
-      if type(sectionValue) == "table" and luna.settings[section] ~= nil then
-        -- Load fields in the section if it has tweakable values
-        if luna.settings[section]['_tweakable'] ~= nil then
-          for settingKey, settingValue in pairs(sectionValue) do
-            -- Only load the key if it's tweakable
-            if tablex.find(luna.settings[section]['_tweakable'], settingKey) ~= nil then
-              luna.settings[section][settingKey] = settingValue
-            end
-          end
-        end
-      end
-    end
-  end
+		-- Iterate over INI sections
+		for section, sectionValue in pairs(playerSettings) do
+			if type(sectionValue) == "table" and luna.settings[section] ~= nil then
+				-- Load fields in the section if it has tweakable values
+				if luna.settings[section]['_tweakable'] ~= nil then
+					for settingKey, settingValue in pairs(sectionValue) do
+						-- Only load the key if it's tweakable
+						if tablex.find(luna.settings[section]['_tweakable'], settingKey) ~= nil then
+							luna.settings[section][settingKey] = settingValue
+						end
+					end
+				end
+			end
+		end
+	end
 end
 
 function Persistence:saveGame()
